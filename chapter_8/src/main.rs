80aa1437fc69b919b3655ea01baf9d3880b0446d
ShivamVarshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-08-04 17:18:49 +0530

    addingfiles

--- chapter_8/src/main.rs
+++ chapter_8/src/main.rs
@@ -1,6 +1,11 @@
 mod vector;
+mod string;
+mod hashmap;
+
 fn main() {
     println!("Hello, world!");
     vector::vectorUse();
     vector::vectorDiffTypes();
+    string::stringUse();
+    hashmap::hashmapUse();
 }
