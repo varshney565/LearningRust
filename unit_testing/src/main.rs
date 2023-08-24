69d433b9dd04a4cecbb0961ba1ce2631fa122fb8
ShivamVarshney <uietcse1923shivamvarshney@kuk.ac.in>
2023-08-24 13:01:37 +0530

    Mocking

---
+++ unit_testing/src/main.rs
@@ -0,0 +1,20 @@
+use mockall::*;
+use mockall::predicate::*;
+#[automock]
+trait MyTrait {
+    fn foo(&self, x: u32) -> u32;
+}
+
+fn call_with_four(x: &impl MyTrait) -> u32 {
+    x.foo(4)
+}
+
+#[test]
+fn check() {
+    let mut mock = MockMyTrait::new();
+    mock.expect_foo()
+    .with(predicate::eq(4))
+    .times(1)
+    .returning(|y| y + 1);
+    assert_eq!(5, call_with_four(&mock));
+}
